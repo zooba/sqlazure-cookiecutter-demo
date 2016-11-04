11cdff536ec04cf0828863b00bd70f295ebfc635
Steve Dower <steve.dower@microsoft.com>
2016-11-04 15:04:42 -0700

    Updates default value and simplifies code

--- {{ cookiecutter.appname }}/program.py
+++ {{ cookiecutter.appname }}/program.py
@@ -1,11 +1,19 @@
 import pyodbc
-import re
 
 CONNECTION_STRING = "{{cookiecutter.connection_string}}"
 
-connection = pyodbc.connect(re.sub('Driver=\{.+?\};', 'Driver={SQL Server};', CONNECTION_STRING))
+connection = pyodbc.connect(CONNECTION_STRING)
+
+# Read all 
 cursor = connection.cursor()
-cursor.execute("select distinct FirstName, LastName from {{cookiecutter.namespace}}.{{cookiecutter.table}} order by LastName, FirstName")
+cursor.execute(
+    "select LastName, Count(*) as 'Members' "
+    "from {{cookiecutter.namespace}}.{{cookiecutter.table}} "
+    "group by LastName "
+    "having Count(*) > 3 "
+    "order by 'Members' DESC")
 
 row = cursor.fetchall()
-print('\n'.join('{0[0]} {0[1]}'.format(r) for r in row))
+print('Family Name        | Members')
+print('-------------------+--------')
+print('\n'.join('{0[0]:<19}|{0[1]:>8}'.format(r) for r in row))
